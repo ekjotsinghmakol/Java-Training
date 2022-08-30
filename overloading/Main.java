c3056824840483d6a924dc571f96761975533b80
ekjotsinghmakol <109962748+ekjotsinghmakol@users.noreply.github.com>
2022-08-30 12:20:45 +0530

    Add files via upload

---
+++ overloading/Main.java
@@ -0,0 +1,13 @@
+package overloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(overloadfunc.add(11,21));
+		System.out.println(overloadfunc.add(11,21,31));
+		System.out.println(overloadfunc.add(11.5,21.8));
+
+	}
+
+}
---
+++ overloading/overloadfunc.java
@@ -0,0 +1,16 @@
+package overloading;
+
+public class overloadfunc {
+	
+	static int add(int x, int y) {
+	return x+y;
+	}
+	
+	static int add (int x, int y, int z) {
+		return x+y+z;
+	}
+	
+	static double add(double x, double y) {
+		return x+y;
+	}
+}
